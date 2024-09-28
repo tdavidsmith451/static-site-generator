82e7880c00b72fd900d2f47713c200edec481337
tdavidsmith451 <tdavidsmith@live.com>
2024-09-27 20:12:58 -0600

    refactored functionality of extract_links and its tests

--- src/extract_links.py
+++ src/extract_links.py
@@ -3,8 +3,8 @@ import re
 
 def extract_markdown_images(text):
     alt_url_list = []
-    alt_text = re.findall(r"(?<=\[).*?(?=\])", text)
-    url = re.findall(r"(?<=\().*?(?=\))", text)
+    alt_text = re.findall(r"(?<=\!\[).*?(?=\])", text)
+    url = re.findall(r"(?<=\()http.*?(?=\))", text)
     for i in range(0, len(alt_text)):
         tuple = (alt_text[i], url[i])
         alt_url_list.append(tuple)
@@ -12,9 +12,9 @@ def extract_markdown_images(text):
 
 def extract_markdown_links(text):
     anchor_url_list = []
-    anchor_text = re.findall(r"(?<=\[).*?(?=\])", text)
-    url = re.findall(r"(?<=\().*?(?=\))", text)
-    for i in range(0, len(alt_text)):
+    anchor_text = re.findall(r"(?<=\[).*?(?=\]\(http)", text)
+    url = re.findall(r"(?<=\()http.*?(?=\))", text)
+    for i in range(0, len(anchor_text)):
         tuple = (anchor_text[i], url[i])
         anchor_url_list.append(tuple)
     return anchor_url_list
--- src/test_extract_links.py
+++ src/test_extract_links.py
@@ -13,7 +13,8 @@ class TestExtractLinks(unittest.TestCase):
 
     def test_extract_markdown_links(self):
         text = "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)"
-        self.assertEqual(extract_markdown_images(text), [("to boot dev", "https://www.boot.dev"), ("to youtube", "https://www.youtube.com/@bootdotdev")])
+        print(extract_markdown_links(text))
+        self.assertEqual(extract_markdown_links(text), [("to boot dev", "https://www.boot.dev"), ("to youtube", "https://www.youtube.com/@bootdotdev")])
 
 
 if __name__ == "__main__":
