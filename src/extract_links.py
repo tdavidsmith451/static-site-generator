c10336cd4bbe37e979e984e7aa865ee618eaf635
tdavidsmith451 <tdavidsmith@live.com>
2024-09-27 19:46:29 -0600

    implemented extract_links and created a test case for both of its functions

---
+++ src/extract_links.py
@@ -0,0 +1,20 @@
+import re
+
+
+def extract_markdown_images(text):
+    alt_url_list = []
+    alt_text = re.findall(r"(?<=\[).*?(?=\])", text)
+    url = re.findall(r"(?<=\().*?(?=\))", text)
+    for i in range(0, len(alt_text)):
+        tuple = (alt_text[i], url[i])
+        alt_url_list.append(tuple)
+    return alt_url_list
+
+def extract_markdown_links(text):
+    anchor_url_list = []
+    anchor_text = re.findall(r"(?<=\[).*?(?=\])", text)
+    url = re.findall(r"(?<=\().*?(?=\))", text)
+    for i in range(0, len(alt_text)):
+        tuple = (anchor_text[i], url[i])
+        anchor_url_list.append(tuple)
+    return anchor_url_list
---
+++ src/test_extract_links.py
@@ -0,0 +1,20 @@
+import unittest
+from extract_links import *
+
+class TestExtractLinks(unittest.TestCase):
+
+    def test_extract_markdown_images(self):
+        text = "This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif) and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg)"
+        self.assertEqual(extract_markdown_images(text), [("rick roll", "https://i.imgur.com/aKaOqIh.gif"), ("obi wan", "https://i.imgur.com/fJRm4Vk.jpeg")])
+
+    def test_extract_markdown_images_2(self):
+        text = "This time there is only one link: ![dog](https://i.pictureofadog.jpeg)"
+        self.assertEqual(extract_markdown_images(text), [("dog", "https://i.pictureofadog.jpeg")])
+
+    def test_extract_markdown_links(self):
+        text = "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)"
+        self.assertEqual(extract_markdown_images(text), [("to boot dev", "https://www.boot.dev"), ("to youtube", "https://www.youtube.com/@bootdotdev")])
+
+
+if __name__ == "__main__":
+    unittest.main()
